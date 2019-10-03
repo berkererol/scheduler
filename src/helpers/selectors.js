4aeb9dabd3aaa11862d92267d39c7f4b303b6764
berkererol <berkererol@gmail.com>
2019-10-03 13:37:10 -0400

    Implemented getAppointmentsForDay function.

---
+++ src/helpers/selectors.js
@@ -0,0 +1,19 @@
+// getAppointmentsForDay fetches appointments for the specific day
+export const getAppointmentsForDay = (state, day) => {
+  const appointment = [];
+
+  const appointmentIds = state.days
+    .filter(item => item.name === day) // to select the specified day's object, which contains id,name,appointments,interviewers,spots keys.
+    .map(item => item.appointments) // to make a copy of the appointments array
+    .reduce((total, value) => total.concat(value), []); // Flattening the array of arrays with reduce (to flatten nested appointments array inside days object)
+
+  if (appointmentIds.length === 0) {
+    return appointment;
+  } else {
+    appointmentIds.forEach(item => {appointment.push(state.appointments[item])})
+    return appointment;
+  }
+}
+
+
+
