b2e4d82f8a1d3a70a377e4c1e4e4f15ed1d02f1b
berkererol <berkererol@gmail.com>
2019-10-02 20:34:32 -0400

    Added Show and Edit components.

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classnames from "classnames";
+
+
+
+export default function DayListItem(props) {
+  const formatSpots = function () {
+    if (!props.spots) {
+      return "no spots remaining"
+    } else return `${props.spots} spots remaining`
+  };
+
+
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected ": props.selected,
+    "day-list__item--full": !props.spots,
+  });
+
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+}
+
+
